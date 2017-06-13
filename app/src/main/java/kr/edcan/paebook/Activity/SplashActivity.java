3dafc5fb97dca22ee092dbe72563763523e41661
Mac <mac@Macui-MacBook-Pro.local>
2017-06-13 14:00:38 +0900

    string resource added

--- app/src/main/java/kr/edcan/paebook/Activity/SplashActivity.java
+++ app/src/main/java/kr/edcan/paebook/Activity/SplashActivity.java
@@ -1,14 +1,34 @@
 package kr.edcan.paebook.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import javax.security.auth.login.LoginException;
+
 import kr.edcan.paebook.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private boolean isSignedUser = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        if(isSignedUser) nextMain();
+        else nextSignIn();
+    }
+
+    private void nextSignIn(){
+        final Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    private void nextMain(){
+        final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
