90733da377a478c92410a2b48031cd41cfb73f7b
kimok0508 <kimok0508@naver.com>
2017-06-22 09:55:40 +0900

    some bug fixed

--- app/src/main/java/kr/edcan/paebook/Activity/DetailActivity.java
+++ app/src/main/java/kr/edcan/paebook/Activity/DetailActivity.java
@@ -86,7 +86,7 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_detail, menu);
-        return true;
+        return Application.uuid == post.getUuid();
     }
 
     @Override
